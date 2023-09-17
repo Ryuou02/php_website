cfaeebd9b6af28203ba530bb578b7fa48d607607
Ryuou02 <133224167+Ryuou02@users.noreply.github.com>
2023-09-17 10:38:51 +0530

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -5,9 +5,9 @@ require_once('connection.php');
 	$uname = $_SESSION["u1"];
 	$pwd = $_SESSION["pwd"];
 	
-	$sql="SELECT * FROM users where username='admin1' and password_hash='".$pwd."';";
+	$sql="SELECT * FROM users where username='admin1' and password_hash=' ? ';";
 	$res = $conn->prepare($sql);
-	$res->execute();
+	$res->execute(array($pwd));
 	$result = $res->fetch();
 	if($result['username']=='')
 	{
