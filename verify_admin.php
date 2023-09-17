6a266dc0bdc217bf445cdb5718ccb9e2932d2837
Ryuou02 <133224167+Ryuou02@users.noreply.github.com>
2023-09-17 10:45:14 +0530

    Update verify_admin.php

--- verify_admin.php
+++ verify_admin.php
@@ -4,9 +4,9 @@ session_start();
 if(isset($_SESSION["u1"]) && isset($_SESSION["pwd"])){
 $u1 = $_SESSION["u1"];
 $pwd = $_SESSION["pwd"];
-$sql="SELECT * FROM users where username='".$u1."' and password_hash='".$pwd."';";
+$sql="SELECT * FROM users where username=? and password_hash=?;";
 	$res = $conn->prepare($sql);
-	$res->execute();
+	$res->execute(array($u1,$pwd));
 	$rnum = $res->fetch();
 	if($rnum['username']==''){
 		die("error in authentication!\n are you sure you're admin?")
